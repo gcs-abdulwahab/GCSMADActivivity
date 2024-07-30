9c0fdded8dc09a41effc55379f0bb5848a7757c6
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-07-30 23:47:29 +0500

    Activities Added

---
+++ app/src/main/java/com/example/gcsmadactivivity/SourceActivity.kt
@@ -0,0 +1,47 @@
+package com.example.gcsmadactivivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gcsmadactivivity.ui.theme.GCSMADActivivityTheme
+
+class SourceActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GCSMADActivivityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Send")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    GCSMADActivivityTheme {
+        Greeting("Android")
+    }
+}
