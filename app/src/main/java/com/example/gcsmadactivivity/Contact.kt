eaf71ecc9ce17fc5797f295520eb20f2b201c748
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-07-30 23:48:49 +0500

    Delete Contact.kt

--- app/src/main/java/com/example/gcsmadactivivity/Contact.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gcsmadactivivity
-
-data class Contact(val id: Int, val name: String){
-    companion object {
-        val contacts = (1..100).map {
-            Contact(it, "Name $it")
-        }
-    }
-}
